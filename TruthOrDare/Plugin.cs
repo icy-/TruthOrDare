115d56b2c8dcedc122781c6bf9a79e38380c8dfb
icy <viper11@gmail.com>
2026-02-22 23:50:51 -0500

    yet more renames

--- TruthOrDare/Plugin.cs
+++ TruthOrDare/Plugin.cs
@@ -22,7 +22,7 @@ public sealed class Plugin : IDalamudPlugin
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("TruthOrDare");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
 
@@ -56,7 +56,7 @@ public sealed class Plugin : IDalamudPlugin
 
         // Add a simple message to the log with level set to information
         // Use /xllog to open the log window in-game
-        // Example Output: 00:57:54.959 | INF | [SamplePlugin] ===A cool log message from Sample Plugin===
+        // Example Output: 00:57:54.959 | INF | [TruthOrDare] ===A cool log message from TruthOrDare===
         Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
     }
 
