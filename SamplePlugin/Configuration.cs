2419a2fd1f9f3142c6aaff65efa47c00d0497137
icy <viper11@gmail.com>
2026-02-22 23:35:50 -0500

    initial rename to TruthOrDare

--- SamplePlugin/Configuration.cs
+++ SamplePlugin/Configuration.cs
@@ -1,7 +1,7 @@
-﻿using Dalamud.Configuration;
+using Dalamud.Configuration;
 using System;
 
-namespace SamplePlugin;
+namespace TruthOrDare;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,12 +1,12 @@
-﻿using Dalamud.Game.Command;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using SamplePlugin.Windows;
+using TruthOrDare.Windows;
 
-namespace SamplePlugin;
+namespace TruthOrDare;
 
 public sealed class Plugin : IDalamudPlugin
 {
